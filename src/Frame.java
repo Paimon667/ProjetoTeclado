94fdc4f82588aa0f56648ba83b6a9a435ed89bbd
mkshiro <48719830+mkshiro@users.noreply.github.com>
2019-05-27 09:58:33 -0300

    Alterações simples e correção de bugs
    Fizemos umas pequenas alterações nas descrições e corrigimos bugs visuais, onde botões apareciam no lugar errado e afins, também teve uma pequena alteração no código.

--- src/Frame.java
+++ src/Frame.java
@@ -44,19 +44,19 @@ public class Frame extends JFrame {
 		inicio.meio.add(acertos, BorderLayout.NORTH);
 
 		testepangrama = new TestePangrama();
-		AcaoRadio aaa = new AcaoRadio(); // TIVE QUE CRIAR A A��O AQUI PARA PODER ALTERAR O LABEL COMPARTILHAVEL ENTRE
+		AcaoRadio aaa = new AcaoRadio(); // TIVE QUE CRIAR A Acao AQUI PARA PODER ALTERAR O LABEL COMPARTILHAVEL ENTRE
 											// AS DUAS CLASSES INICIO E TESTEPAN
 		for (int i = 0; i < testepangrama.ChecPangrama.length; i++) {
 			testepangrama.ChecPangrama[i].addItemListener(aaa);
 		}
-
+		
 		AcaoBotoes b = new AcaoBotoes();
 		inicio.cima.addKeyListener(b);
 
 		abas = new JTabbedPane();
 		abas.addTab("Principal", inicio);
 		abas.addTab("Teste Pangrama", testepangrama);
-		abas.addTab("Histórico", historico);
+		abas.addTab("Historico", historico);
 		this.add(abas);
 	}
 
@@ -90,29 +90,34 @@ public class Frame extends JFrame {
 
 		}
 
-		public void keyPressed(KeyEvent e) {  // primeiro checa se encontrou o botão, caso não, lança uma exceção e da erro
-			try {							  // ARMAZENA A POSIÇÃO DO BOTAO NA VARIAVEL VARIAVEL	
+		public void keyPressed(KeyEvent e) {  // primeiro checa se encontrou o botao, caso nao, lanca uma excecao e da erro
+			try {							  // ARMAZENA A POSICAO DO BOTAO NA VARIAVEL VARIAVEL	
 				int variavel = inicio.ProcurarBotao(e);
 				
 				
-				
+											// SE A VARIAVEL RETORNAR O VALOR -1 ELE LANCA A EXCECAO 
 				if (variavel < 0) {			//CHECA SE O PANGRAMA TERMINOU (SE A TECLA ATUAL FOR MAIOR QUE O TAMANHO DELE)
 					throw new Exception();	//MUDA A COR DE PRETO CASO ACERTE, VERMELHO CASO ERRE
-				}				// DESCONSIDERA CAPS LOCK E SHIFT NA CONTAGEM DE ACERTOS E ERROS E TECLA ATUAL
+				}				            // DESCONSIDERA CAPS LOCK E SHIFT NA CONTAGEM DE ACERTOS E ERROS E TECLA ATUAL
 				
 				
 				
 				
-				if(variavel==40) {          // SE EU APERTEI ENTER
-					if(panDigitar.getText()!=panPadrao) {
-					TeclaAtual=999;
+				if(variavel==40) {                          // SE EU APERTEI ENTER
+					if(panDigitar.getText()!=panPadrao) {   // SE APERTEI ENTER NUM PANGRAMA DIFERENTE DO PADRAO
+						if(panDigitar.getText().length()!=TeclaAtual) {
+							TeclaAtual=999;       //SE O ENTER NAO FOI PRESSIONADO NO FINAL DA FRASE
+						}
 					}
 					
-				}   // SE A TECLA NÃO FOR ENTER
+				}   
 				
 				
-				if (TeclaAtual < panDigitar.getText().length() && panDigitar.getText()!=panPadrao) { 
-					if (e.getKeyChar() == panDigitar.getText().charAt(TeclaAtual)) {//INCREMENTA A TECLA ATUAL A CADA TENTATIVA
+				if (TeclaAtual < panDigitar.getText().length() && panDigitar.getText()!=panPadrao) {  
+					// SE AINDA N TERMINOU DE DIGITAR O PANGRAMA, E ELE NAO Eh O PADRAO
+					//INCREMENTA A TECLA ATUAL A CADA TENTATIVA
+					if (e.getKeyChar() == panDigitar.getText().charAt(TeclaAtual)) {
+						//SE A TECLA FOR IGUAL AO CHAR DA POSICAO ATUAL DO PANGRAMA
 						inicio.botoes[variavel].setBackground(Color.BLACK);
 						inicio.cima.setText(inicio.cima.getText()+e.getKeyChar());
 						TeclaAtual++;
@@ -120,10 +125,13 @@ public class Frame extends JFrame {
 						}
 			
 
-					 else {
+					 else { //SE A TECLA ATUAL NAO FOR IGUAL AO DA POSICAO ATUAL DO PANGRAMA
 						if (variavel == 28 || variavel == 41 || variavel == 13) {
+							//SE ESSA TECLA FOR SHIFT, BACKSPACE E CAPS LOCK ELE NAO DA ERRO, SOMENTE FAZ SUA FUNCAO
 							inicio.botoes[variavel].setBackground(Color.black);
 							if(variavel==13) {
+							//SE FOR BACSKPACE ELE APAGA O DIGITO ANTERIOR E DECREMENTA A TECLA ATUAL DO PANGRAMA
+							// NAO DESCONSIDERA O ERRO CASO TENHA OCORRIDO, MESMO TERMINANDO O PANGRAMA, VAI DAR DIGITADO INCORRETAMENTE
 								inicio.cima.setText(inicio.cima.getText().substring(0, inicio.cima.getText().length()-1));
 								TeclaAtual--;
 							}
@@ -140,25 +148,29 @@ public class Frame extends JFrame {
 				
 				else {
 					
-					if(TeclaAtual>0) {
+					if(TeclaAtual>0) { 
+						System.out.println(TeclaAtual);
+						//SE O TAMANHO DA FRASE DIGITADA FOR IGUAL DO PANGRAMA E NAO HOUVE ERRO, ELE DIZ FRASE CORRETA
+						// ELE SO VEM PRA CA CASO ENTER SEJA PRESSIONADO (QUE SETA VALOR 999 A TeclaAtual) ou caso termine o pangrama
 						acertos.setText("Acertos: "+Acertos+" Erros: "+Erros);
-						String extra=(Erros==0 && TeclaAtual==panDigitar.getText().length())? " A frase foi digitada corretamente":" A frase não foi digitada corretamente";
+						String extra=(Erros==0 && TeclaAtual==panDigitar.getText().length())? " A frase foi digitada corretamente":" A frase nao foi digitada corretamente";
 						acertos.setText(acertos.getText()+extra);
 						historico.Escrever(inicio.cima.getText());
 						
-					}
+					
 					panDigitar.setText(panPadrao);
 					TeclaAtual = 0;
 					inicio.cima.setText("");
 					Acertos=0;
 					Erros=0;
+					}
 					
 				}
 
 			}
 
 			catch (Exception x) {
-				JOptionPane.showMessageDialog(null, "Tecla Inválida !!!", "Error", JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(null, "Tecla Invalida !!!", "Error", JOptionPane.ERROR_MESSAGE);
 			}
 
 		}
@@ -182,7 +194,7 @@ public class Frame extends JFrame {
 	public static void main(String[] args) {
 		Frame f = new Frame();
 		f.setVisible(true);
-		f.setSize(750, 380);
+		f.setSize(750, 420);
 		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
 		f.setResizable(true);
 	}
